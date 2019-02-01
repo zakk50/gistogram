f3095983a976584ef068442bde877c0f4589204d
zakk <zakk50@mail.ru>
2019-02-01 13:35:14 +0300

    01.02

--- src/components/taskbar/Sidebar.jsx
+++ src/components/taskbar/Sidebar.jsx
@@ -40,7 +40,8 @@ const Sidebar = createReactClass({
   render() {
     return(
       <div className="sidebar">
-        <div>
+      {/* !!! Добавление группы */}
+        {/* <div>
           <h2>ДОБАВИТЬ ГРУППУ</h2>
         </div>
         <ul>
@@ -55,10 +56,19 @@ const Sidebar = createReactClass({
               <span className="plus_" >+</span>
             </div>
           </li>
-        </ul>
+        </ul> */}
+        {/* !!! End Добавление группы */}
 
         <div>
           <h2>ДОБАВИТЬ ЭЛЕМЕНТ</h2>
+          <div className=""></div>
+          <input type="text"/>
+          <input type="text"/>
+          <input type="time"/>
+          {/* <div class="col">
+            <input className="effect-2" type="text" placeholder="Placeholder11 Text" />
+              <span className="focus-border"></span>
+          </div> */}
         </div>
 
         <div className="NoteEditor__footer">
@@ -67,20 +77,96 @@ const Sidebar = createReactClass({
             onChange={this.handleColorChange}
           />
         </div>
-
-        <div className="block-task">
-          <div className="block color-element"></div>
-          <div className="block task-number"><p>11</p></div>
-          <div className="block task-text clearfix">
-            <div className="task-test-tema"><p>Lakta</p></div>
-            <div className="task-text-time"><p>12:00</p></div>
+        <div className="prokrutka">
+          <div className="block-task">
+            <div className="block color-element"></div>
+            <div className="block task-number"><p>8</p></div>
+            <div className="block task-text clearfix">
+              <div className="task-text-tema"><p>Object</p></div>
+              <div className="task-text-time"><p>12:00</p></div>
+              <div className="task-text-text">Next next next next next</div>
+            </div>
+            <div className="block task-delete">
+              {/* <div className="button-delete"><img src="http://localhost:3000/public/del-button.png"></img></div> */}
+            </div>
           </div>
-          <div className="block task-delete"></div>
-        </div>
 
-        <div className="button-create" >
-          <span className="plus_ss" >+</span>
+          {/* для вида */}
+          <div className="block-task">
+            <div className="block color-element e1"></div>
+            <div className="block task-number"><p>7</p></div>
+            <div className="block task-text clearfix">
+              <div className="task-text-tema"><p>Object</p></div>
+              <div className="task-text-time"><p>12:00</p></div>
+              <div className="task-text-text">Next next next next next</div>
+            </div>
+            <div className="block task-delete"></div>
+          </div>
+          <div className="block-task">
+            <div className="block color-element e2"></div>
+            <div className="block task-number"><p>6</p></div>
+            <div className="block task-text clearfix">
+              <div className="task-text-tema"><p>Object</p></div>
+              <div className="task-text-time"><p>12:00</p></div>
+              <div className="task-text-text">Next next next next next</div>
+            </div>
+            <div className="block task-delete"></div>
+          </div>
+          <div className="block-task">
+            <div className="block color-element e3"></div>
+            <div className="block task-number"><p>5</p></div>
+            <div className="block task-text clearfix">
+              <div className="task-text-tema"><p>Object</p></div>
+              <div className="task-text-time"><p>12:00</p></div>
+              <div className="task-text-text">Next next next next next</div>
+            </div>
+            <div className="block task-delete"></div>
+          </div>
+          <div className="block-task">
+            <div className="block color-element e4"></div>
+            <div className="block task-number"><p>4</p></div>
+            <div className="block task-text clearfix">
+              <div className="task-text-tema"><p>Object</p></div>
+              <div className="task-text-time"><p>12:00</p></div>
+              <div className="task-text-text">Next next next next next</div>
+            </div>
+            <div className="block task-delete"></div>
+          </div>
+          <div className="block-task">
+            <div className="block color-element e5"></div>
+            <div className="block task-number"><p>3</p></div>
+            <div className="block task-text clearfix">
+              <div className="task-text-tema"><p>Object</p></div>
+              <div className="task-text-time"><p>12:00</p></div>
+              <div className="task-text-text">Next next next next next</div>
+            </div>
+            <div className="block task-delete"></div>
+          </div>
+          <div className="block-task">
+            <div className="block color-element e6"></div>
+            <div className="block task-number"><p>2</p></div>
+            <div className="block task-text clearfix">
+              <div className="task-text-tema"><p>Object</p></div>
+              <div className="task-text-time"><p>12:00</p></div>
+              <div className="task-text-text">Next next next next next</div>
+            </div>
+            <div className="block task-delete"></div>
+          </div>
+          <div className="block-task">
+            <div className="block color-element e7"></div>
+            <div className="block task-number"><p>1</p></div>
+            <div className="block task-text clearfix">
+              <div className="task-text-tema"><p>Object</p></div>
+              <div className="task-text-time"><p>12:00</p></div>
+              <div className="task-text-text">Next next next next next</div>
+            </div>
+            <div className="block task-delete"></div>
+          </div>
         </div>
+        {/* концовка для вида */}
+        {/* <div className="button-create" >
+          <span className="plus_ss" >+</span>
+        </div> */}
       </div>
     );
   }
